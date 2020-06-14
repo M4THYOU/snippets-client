b5499b7c4f4fb843b061c325d58df49c29d8a425
Matthew Wolfe <m5wolfe@edu.uwaterloo.ca>
2020-06-14 11:00:13 -0400

    determine which api to use

--- src/api/config.js
+++ src/api/config.js
@@ -1,12 +1,22 @@
 const EndpointsEnum = Object.freeze({
-    'local':'http://localhost:3000/api/'
+    'local':'http://localhost:3000/api/',
+    'deploy': 'https://snippets-api-v1.herokuapp.com/api/'
 });
 const ApiVersionsEnum = Object.freeze({
     'V1':'v1/'
 });
 
+let base;
+// eslint-disable-next-line no-restricted-globals
+switch (location.hostname) {
+    case 'localhost':
+        base = EndpointsEnum.local;
+        break
+    default:
+        base = EndpointsEnum.deploy;
+}
+
 // Start config
-const base = EndpointsEnum.local;
 const version = ApiVersionsEnum.V1;
 // End config
 
